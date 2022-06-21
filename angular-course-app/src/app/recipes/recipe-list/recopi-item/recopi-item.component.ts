fe83f5946a79705a0e424857588626b1b758d703
ssun97 <sunmingyu.97@gmail.com>
2022-06-21 14:41:46 -0500

    init the project and prepare the components

---
+++ angular-course-app/src/app/recipes/recipe-list/recopi-item/recopi-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recopi-item',
+  templateUrl: './recopi-item.component.html',
+  styleUrls: ['./recopi-item.component.css']
+})
+export class RecopiItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
