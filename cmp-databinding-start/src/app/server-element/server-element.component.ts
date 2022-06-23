bc81e69812001839ffb854756da2c0fbdfe57945
ssun97 <sunmingyu.97@gmail.com>
2022-06-23 17:08:52 -0500

    Practice component cycle

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewEncapsulation,
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterViewInit,
+  OnDestroy,
+  ViewChild,
+  ElementRef,
+  ContentChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
-  encapsulation: ViewEncapsulation.Emulated,// None, Native
+  encapsulation: ViewEncapsulation.Emulated, // None, Native
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterViewInit, OnDestroy {
   @Input() element: { type: string; name: string; content: string };
-
-  constructor() {}
-
-  ngOnInit(): void {}
+  @Input() name: string;
+  @ViewChild('heading', { static: true }) header: ElementRef;
+  @ContentChild('contentParagraph', { static: true }) paragraph: ElementRef;
+  constructor() {
+    console.log('ServerElementComponent constructor');
+  }
+  ngOnDestroy(): void {
+    console.log('ServerElementComponent ngOnDestroy');
+  }
+  ngAfterViewInit(): void {
+    // throw new Error('Method not implemented.');
+    console.log('ServerElementComponent ngAfterViewInit');
+    console.log(this.header.nativeElement.textContent);
+  }
+  ngAfterContentInit(): void {
+    // throw new Error('Method not implemented.');
+    console.log('ServerElementComponent ngAfterContentInit');
+    console.log(this.paragraph.nativeElement.textContent);
+  }
+  ngDoCheck(): void {
+    // throw new Error('Method not implemented.');
+    console.log('ServerElementComponent ngDoCheck');
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ServerElementComponent ngOnChanges');
+    console.log(changes);
+  }
+  ngOnInit(): void {
+    console.log('ServerElementComponent ngOnInit');
+    console.log(this.header.nativeElement.textContent);
+    console.log(this.paragraph.nativeElement.textContent);
+  }
 }
